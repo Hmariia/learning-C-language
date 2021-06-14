aa997bf7ea2a0728e79b6357fb2f4b6db3e31bfe
Hmariia <85872157+Hmariia@users.noreply.github.com>
2021-06-14 04:24:12 -0700

    lib

---
+++ ft_list.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkrokis <mkrokis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/08 16:06:42 by mkrokis           #+#    #+#             */
+/*   Updated: 2021/06/09 16:14:52 by mkrokis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct			s_list
+{
+	struct s_list		*next;
+	void				*data;
+}						t_list;
+
+t_list					*ft_create_elem(void *data);
+#endif
