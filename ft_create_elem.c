b6de59424846d015ddec31033adf082baf4ed7fc
Hmariia <85872157+Hmariia@users.noreply.github.com>
2021-06-14 04:23:57 -0700

    function create_el()

---
+++ ft_create_elem.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkrokis <mkrokis@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/08 12:04:48 by mkrokis           #+#    #+#             */
+/*   Updated: 2021/06/09 18:53:20 by mkrokis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list *list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->data = data;
+		list->next = NULL;
+	}
+	return (list);
+}
